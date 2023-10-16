0fbed8be54d81694f1eda0436a2a3f6eec9a995e
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-16 18:04:05 +0200

    Add project files.

---
+++ 8. Accept Two Integer And Return/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Accept_Two_Integer_And_Return
+{
+    internal class Program
+    {
+        static bool CheckConditions(int num1, int num2)
+        {
+            return (num1 == 5 || num2 == 5 || num1 + num2 == 5 || Math.Abs(num1 - num2) == 5);
+        }
+        static void Main(string[] args)
+        {
+            int[,] testCases = {
+              {5, 4},
+              {4, 3},
+              {1, 4}
+        };
+
+            for (int i = 0; i < testCases.GetLength(0); i++)
+            {
+                int num1 = testCases[i, 0];
+                int num2 = testCases[i, 1];
+
+                bool result = CheckConditions(num1, num2);
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }    }
+    }
+}
